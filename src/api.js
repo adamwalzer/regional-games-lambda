e34436e338b0e31e1f3fc1117f514fa216419835
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2017-03-06 10:43:35 -0500

    Completed PoC and created job process in processor

--- src/api.js
+++ src/api.js
@@ -1,107 +1,115 @@
+var _ = require('lodash');
 var request = require('request');
-var _       = require('lodash');
-var logger  = require('./logger.js').logger;
 
 /**
- * Builds an API request
+ * Marshals calls to the API
  *
- * @type {Function}
- * @param {Object} options - Options for the API request
- *
- * @return {request}
+ * @module api
+ * @param {String} apiUri - the base path for the API
+ * @param {Object} options - key value for the
+ * @param {Object.<logger>} logger - Logger
+ * @return {{apiRequest, postRequest}}
  */
-var buildApi = _.memoize((options) => {
-    var apiOptions = _.defaults(options, { timeout: 3000, json: true, strictSsl: true });
-    if (apiOptions.auth == null ||
-        apiOptions.auth.user == null ||
-        apiOptions.auth.password == null
+module.exports = function (apiUri, options, logger) {
+    logger.info('debug', 'Initializing API');
+    _.defaults(options, {});
+
+    options.timeout = 3000;
+    options.json = true;
+    options.strictSsl = true;
+
+    if (options.auth == null ||
+        options.auth.user == null ||
+        options.auth.password == null
     ) {
         throw Error('Cannot make api requests with missing options');
     }
 
-    return request.defaults(apiOptions);
-});
+    var requester = request.defaults(options);
 
-/**
- * Makes a get call to the API
- *
- * @param request
- * @param base
- * @param path
- * @param query
- * @param resolve
- * @param reject
- * @return {Promise}
- */
-var makeApiGet = (request, base, path, query, resolve, reject) => {
-    query      = _.defaults(query, {});
-    path       = _.startsWith('/', path) ? path : '/' + path;
-    var apiUrl = base + path;
-    return new Promise(function (apiResolve, apiReject) {
-        request(base + path, { qs: query }, (err, response, body) => {
-            if (err) {
-                return apiReject(Error('Error requesting: ' + apiUrl + ' ' + err));
-            }
+    /**
+     * Makes a get call to the API
+     *
+     * @param {Object.<request>} request - Request Module
+     * @param base
+     * @param path
+     * @param query
+     * @param resolve
+     * @param reject
+     * @return {Promise}
+     */
+    var makeApiGet = (request, base, path, query, resolve, reject) => {
+        _.defaults(query, {});
+        path = _.startsWith('/', path) ? path : '/' + path;
+        var apiUrl = base + path;
+        logger.log('debug', 'Making call to:' , apiUrl, 'with the following query:', query);
+        return new Promise(function (apiResolve, apiReject) {
+            request(base + path, {qs: query}, (err, response, body) => {
+                if (err) {
+                    return apiReject(Error('Error requesting: ' + apiUrl + ' ' + err));
+                }
 
-            if (response.statusCode !== 200) {
-                return apiReject(Error('Invalid response code: ' + response.statusCode));
-            }
+                if (response.statusCode !== 200) {
+                    return apiReject(Error('Invalid response code: ' + response.statusCode));
+                }
 
-            if (_.isEmpty(body)) {
-                return apiReject(Error('Empty response body from: ' + apiUrl));
-            }
+                if (_.isEmpty(body)) {
+                    return apiReject(Error('Empty response body from: ' + apiUrl));
+                }
 
-            return apiResolve(body);
-        });
-    })
-        .then(body => {
-            logger.log('debug', 'Completed request to:', apiUrl);
-            _.attempt(resolve, body);
+                return apiResolve(body);
+            });
         })
-        .catch(function (err) {
-            logger.log('error', err);
-            _.attempt(reject, [err]);
-            throw err;
-        });
-};
+            .then(body => {
+                logger.log('debug', 'Completed request to:', apiUrl);
+                _.attempt(resolve, body);
+            })
+            .catch(function (err) {
+                logger.log('error', err);
+                _.attempt(reject, [err]);
+                throw err;
+            });
+    };
 
-/**
- * Makes a Post call to the API
- *
- * @param request
- * @param base
- * @param path
- * @param data
- * @param resolve
- * @param reject
- * @return {Promise}
- */
-var makeApiPost = (request, base, path, data, resolve, reject) => {
-    path       = _.startsWith('/', path) ? path : '/' + path;
-    var apiUrl = base + path;
-    return new Promise(function (apiResolve, apiReject) {
-        request(base + path, { method: 'POST', json: data }, (err, response, body) => {
-            if (err) {
-                return apiReject(Error('Error posting: ' + apiUrl + ' ' + err));
-            }
+    /**
+     * Makes a Post call to the API
+     *
+     * @param {Object.<request>} request - Request Module
+     * @param {String} base
+     * @param {String} path
+     * @param {Object} data
+     * @param resolve
+     * @param reject
+     * @return {Promise}
+     */
+    var makeApiPost = (request, base, path, data, resolve, reject) => {
+        path = _.startsWith('/', path) ? path : '/' + path;
+        var apiUrl = base + path;
+        logger.log('debug', 'Posting to:' , apiUrl, 'with the following data:', data);
+        return new Promise(function (apiResolve, apiReject) {
+            request(base + path, {method: 'POST', json: data}, (err, response, body) => {
+                if (err) {
+                    return apiReject(Error('Error posting: ' + apiUrl + ' ' + err));
+                }
 
-            // TODO CORE-3409 is resolved then this can check 500 errors then reject
+                // TODO CORE-3409 is resolved then this can check 500 errors then reject
 
-            return apiResolve(response);
-        });
-    })
-        .then((body) => {
-            logger.log('debug', 'Completed POST request to:', apiUrl);
-            _.attempt(resolve, body);
+                return apiResolve(response);
+            });
         })
-        .catch(function (err) {
-            logger.log('error', err);
-            _.attempt(reject, [err]);
-            throw err;
-        });
-};
-module.exports  = {
-    buildApi,
-    makeApiGet,
-    makeApiPost
+            .then((body) => {
+                logger.log('debug', 'Completed POST request to:', apiUrl);
+                _.attempt(resolve, body);
+            })
+            .catch(function (err) {
+                logger.log('error', err);
+                _.attempt(reject, [err]);
+                throw err;
+            });
+    };
+
+    return {
+        apiRequest: _.partial(makeApiGet, requester, apiUri),
+        postRequest: _.partial(makeApiPost, requester, apiUri)
+    }
 };
--- src/logger.js
+++ src/logger.js
@@ -1,24 +1,24 @@
-var winston       = require('winston');
-var rollbar       = require('winston-rollbar').Rollbar;
-var rollbarToken  = process.env.ROLLBAR_TOKEN;
+var winston = require('winston');
+var rollbar = require('winston-rollbar').Rollbar;
+var rollbarToken = process.env.ROLLBAR_TOKEN;
 var rollbarConfig = {
     environment: "local",
-    level:       "debug",
-    enabled:     false
+    level: "error",
+    enabled: false
 };
 
 var logger = new (winston.Logger)({
     exitOnError: false,
-    level:       'info',
-    transports:  [
+    level: 'error',
+    transports: [
         new (winston.transports.Console)({
             colorize: true
         }),
         new (winston.transports.Rollbar)({
             rollbarAccessToken: rollbarToken,
-            rollbarConfig:      rollbarConfig,
-            level:              rollbarConfig.level,
-            handleExceptions:   true
+            rollbarConfig: rollbarConfig,
+            level: rollbarConfig.level,
+            handleExceptions: true
         })
     ]
 });
